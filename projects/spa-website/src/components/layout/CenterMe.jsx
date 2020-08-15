cb73273722017896ee3a04997fc5262075aa001c
Zhenxi <PiscesXP@outlook.com>
2020-08-15 14:15:43 +0800

    调整响应式居中组件断点

--- projects/spa-website/src/components/layout/CenterMe.jsx
+++ projects/spa-website/src/components/layout/CenterMe.jsx
@@ -12,7 +12,7 @@ function CenterMe(props) {
 function CenterMeResponsive(props) {
   return (
     <Row gutter={[8, 0]} type="flex" justify="center" align="top">
-      <Col xs={24} sm={24} md={24} lg={16} xl={12}>
+      <Col xs={24} sm={24} md={22} lg={20} xl={17} xxl={14}>
         {props.children}
       </Col>
     </Row>
