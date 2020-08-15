298e239702395bce9f900fe4b0a7e85e566d0f36
Zhenxi <PiscesXP@outlook.com>
2020-08-15 19:42:42 +0800

    上传附件时可以预览附件图片

--- projects/spa-website/src/components/attachment/AttachmentUpload.jsx
+++ projects/spa-website/src/components/attachment/AttachmentUpload.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import { Form, Icon, Upload } from "antd";
+import React, { useState } from "react";
+import { Form, Icon, Modal, Upload } from "antd";
 import { config } from "CONFIG";
 import "./AttachmentUpload.css";
 
 const actionUrl =
   config.network.api.protocol + "://" + config.network.api.host + "/upload";
+
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
 
 function AttachmentUpload({
   label,
@@ -12,32 +21,58 @@ function AttachmentUpload({
   id = "attachments",
   listType = "picture-card",
 }) {
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [previewImage, setPreviewImage] = useState(null);
+
+  async function handlePreview(file) {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.preview);
+    setPreviewVisible(true);
+  }
+
+  function cancelPreview() {
+    setPreviewVisible(false);
+  }
+
   return (
-    <Form.Item label={label}>
-      {getFieldDecorator(id, {
-        valuePropName: "fileList",
-        getValueFromEvent: (e) => {
-          if (Array.isArray(e)) {
-            return e;
-          }
-          return e && e.fileList;
-        },
-      })(
-        <Upload
-          action={actionUrl}
-          headers={{
-            "X-Requested-With": null,
-          }}
-          withCredentials
-          listType={listType}
-        >
-          <div>
-            <Icon type="plus" />
-            <div className="ant-upload-text">上传附件</div>
-          </div>
-        </Upload>
-      )}
-    </Form.Item>
+    <div>
+      <Form.Item label={label}>
+        {getFieldDecorator(id, {
+          valuePropName: "fileList",
+          getValueFromEvent: (e) => {
+            if (Array.isArray(e)) {
+              return e;
+            }
+            return e && e.fileList;
+          },
+        })(
+          <Upload
+            action={actionUrl}
+            headers={{
+              "X-Requested-With": null,
+            }}
+            withCredentials
+            listType={listType}
+            onPreview={handlePreview}
+          >
+            <div>
+              <Icon type="plus" />
+              <div className="ant-upload-text">上传附件</div>
+            </div>
+          </Upload>
+        )}
+      </Form.Item>
+      <Modal
+        centered
+        visible={previewVisible}
+        footer={null}
+        onCancel={cancelPreview}
+      >
+        <img alt="preview" style={{ width: "100%" }} src={previewImage} />
+      </Modal>
+    </div>
   );
 }
 
