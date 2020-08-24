8b4f371ab60583fc36300f671914bb452920120f
Zhenxi <PiscesXP@outlook.com>
2020-08-24 20:15:07 +0800

    非nju.itxia.cn时，提示跳转过去

--- projects/spa-website/src/page/wcms/WCMS.js
+++ projects/spa-website/src/page/wcms/WCMS.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { UserInfoProvider } from "CONTEXT/UserInfo";
 import { routePath } from "../routePath";
-import { BackTop, Col, Layout, Row } from "antd";
+import { BackTop, Col, Layout, Modal, Row } from "antd";
 import { Footer as MyFooter } from "COMPONENTS/footer";
 import { WcmsNavigateBar } from "COMPONENTS/navigate";
 import { NotFound } from "COMPONENTS/notFound";
@@ -12,6 +12,7 @@ import { HandleOrder } from "./handleOrder";
 import { Profile } from "./profile";
 import { AnnouncementManage } from "./announcementManage";
 import { MemberManage } from "./memberManage";
+import { useLocalStorageState } from "HOOK";
 
 const { Header, Content, Footer } = Layout;
 
@@ -19,6 +20,33 @@ const { Header, Content, Footer } = Layout;
  * 后台系统的router.
  * */
 function WCMS() {
+  //提示是否前往 nju.itxia.cn
+  const [noticeGotoNewSite, setNoticeGotoNewSite] = useLocalStorageState({
+    key: "noticeGotoNewSite",
+    init: () => {
+      return window.location.host !== "nju.itxia.cn";
+    },
+  });
+
+  useEffect(() => {
+    if (noticeGotoNewSite) {
+      Modal.info({
+        title: "试试新网站？",
+        content: "新网站(https://nju.itxia.cn)上线了，速度更快，功能更全.",
+        centered: true,
+        okText: "现在就去",
+        onOk: () => {
+          window.location = "https://nju.itxia.cn";
+        },
+        cancelText: "不用了",
+        onCancel: () => {
+          setNoticeGotoNewSite(false);
+        },
+        okCancel: true,
+      });
+    }
+  }, [noticeGotoNewSite, setNoticeGotoNewSite]);
+
   return (
     <Switch>
       <Route exact path={routePath.WCMS}>
