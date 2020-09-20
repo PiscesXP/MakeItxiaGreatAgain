b2c361cad63e7849a6d246120dd32a3f263cffcc
Zhenxi <PiscesXP@outlook.com>
2020-09-20 20:20:57 +0800

    重构useLocalStorageState

--- projects/spa-website/src/hook/useLocalStorageState.js
+++ projects/spa-website/src/hook/useLocalStorageState.js
@@ -1,48 +1,45 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 /**
- * @param key {string}
- * @param init {function|Object?}
- * @param transform {function}
+ * 使用LocalStorage保持的值作为state.
+ * 更新时也会写入LocalStorage.
+ * @param key {string} LocalStorage key
+ * @param init {function|Object?} Initial value, or function returns initial value
+ * @param transform {function} transform state while set new value
  * */
-function useLocalStorageState({ key, init = null, transform = (a) => a }) {
-  const [state, setState] = useState(() => {
-    //从localStorage中读取初始值，若不存在则使用init
-    function getInit() {
-      let initValue;
-      if (typeof init === "function") {
-        initValue = init();
-      } else {
-        initValue = init;
-      }
-      window.localStorage.setItem(key, JSON.stringify(initValue));
-      return initValue;
+function useLocalStorageState({ key, init = null, transform = null }) {
+  const [state, setState] = useState(getInitValue());
+
+  /**
+   * 从LocalStorage获取初始值.
+   * 若不存在，则使用init的值.
+   * @return initial value
+   * */
+  function getInitValue() {
+    const value = localStorage.getItem(key);
+    try {
+      return JSON.parse(value);
+    } catch (e) {}
+    if (typeof init === "function") {
+      return init();
     }
-    const rawValue = window.localStorage.getItem(key);
-    if (rawValue === null || rawValue === "undefined") {
-      return getInit();
+    return init;
+  }
+
+  function updateState(newState) {
+    if (newState === undefined) {
+      localStorage.removeItem(key);
+      setState(undefined);
     } else {
-      try {
-        return JSON.parse(rawValue);
-      } catch (e) {
-        return getInit();
+      if (typeof transform === "function") {
+        newState = transform(newState);
       }
-    }
-  });
-
-  const set = useCallback(
-    (newState) => {
-      if (newState !== undefined) {
-        window.localStorage.setItem(key, JSON.stringify(transform(newState)));
-      } else {
-        window.localStorage.removeItem(key);
-      }
+      localStorage.setItem(key, JSON.stringify(newState));
       setState(newState);
-    },
-    [key, transform, setState]
-  );
+    }
+  }
 
-  return [state, set];
+  return [state, updateState];
 }
 
 export { useLocalStorageState };
