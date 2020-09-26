3ce0fc815e6f941e24fbde08161c93b5e5471c6d
Zhenxi <PiscesXP@outlook.com>
2020-09-26 12:53:56 +0800

    usePersistFn允许传入undefined

--- projects/spa-website/src/hook/usePersisFn.ts
+++ projects/spa-website/src/hook/usePersisFn.ts
@@ -5,14 +5,21 @@ declare type noop = (...args: any[]) => any;
 /**
  * 保证函数调用地址不发生变化.
  * */
-function usePersistFn<T extends noop>(fn: noop) {
+function usePersistFn<T extends noop>(fn: noop | undefined) {
   const ref = useRef<any>(() => {
     throw new Error("Cannot call function while rendering.");
   });
 
   ref.current = fn;
 
-  return useCallback(((...args) => ref.current(...args)) as T, []);
+  return useCallback(
+    ((...args) => {
+      if (typeof ref.current === "function") {
+        return ref.current(...args);
+      }
+    }) as T,
+    []
+  );
 }
 
 export { usePersistFn };
