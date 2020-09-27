c28200facd129823bacf5b1e75be62ad0f9180a0
Zhenxi <PiscesXP@outlook.com>
2020-09-27 15:51:24 +0800

    Add antd ConfigProvider.

--- projects/spa-website/src/App.tsx
+++ projects/spa-website/src/App.tsx
@@ -7,9 +7,15 @@ import "antd/dist/antd.css";
 import "./App.css";
 
 import { Main } from "@/page";
+import { ConfigProvider } from "antd";
+import zhCN from "antd/es/locale/zh_CN";
 
 function App() {
-  return <Main />;
+  return (
+    <ConfigProvider locale={zhCN}>
+      <Main />
+    </ConfigProvider>
+  );
 }
 
 export { App };
