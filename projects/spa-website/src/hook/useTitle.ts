68e4cc170756313ff6d619704f532a7d3b8b4cd2
Zhenxi <PiscesXP@outlook.com>
2020-12-07 10:34:09 +0800

    Update useTitle.ts

--- projects/spa-website/src/hook/useTitle.ts
+++ projects/spa-website/src/hook/useTitle.ts
@@ -1,27 +1,15 @@
-import { useEffect, useRef } from "react";
-
 /**
  * 更改标题.
  * */
-function useTitle(title: string, recoverOnUnmount: boolean = true) {
-  const ref = useRef<string>(title);
+function useTitle(title: string) {
   document.title = title;
-
-  useEffect(() => {
-    const initialTitle = ref.current;
-    return () => {
-      if (recoverOnUnmount) {
-        document.title = initialTitle;
-      }
-    };
-  }, [recoverOnUnmount]);
 }
 
-function useTitleWCMS(prefix: string, recoverOnUnmount?: boolean) {
-  return useTitle(prefix + " - IT侠后台管理系统", recoverOnUnmount);
+function useTitleWCMS(prefix: string) {
+  return useTitle(prefix + " - IT侠后台管理系统");
 }
 
-function useTitleCustom(prefix: string, recoverOnUnmount?: boolean) {
+function useTitleCustom(prefix: string) {
   return useTitle(prefix + " - IT侠预约系统");
 }
 
